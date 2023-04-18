c6ce74d09eb5a5a5e6019ca2e16230de235a03a4
Don Reyes <92570582+Donreyes28@users.noreply.github.com>
2023-04-18 22:37:11 +0800

    Update s3.js

--- utils/s3.js
+++ utils/s3.js
@@ -1,38 +1,10 @@
-// const { AWS } = require('aws-sdk');
-// const fileSaver = require('file-saver');
-
-// AWS.config.update({
-//   accessKeyId: 'AKIAXGGJDX5IK3Z6DIF7',
-//   secretAccessKey: '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',
-// });
-
-// const BUCKET = 'cpi-warehouse-tools';
-// const REGION = 'us-east-1';
-
-// exports.readFileFromS3 = (fileName) => {
-//     const s3 = new AWS.S3();
-    // new Promise(async (resolve, reject) => {
-    //     try {
-    //         const params = {
-    //             Bucket: BUCKET,
-    //             Key: `${fileName}`
-    //         };
-    //         const stream = await s3.getObject(params).createReadStream();
-    //         resolve(stream);
-    //     } catch (error) {
-    //         reject(error);
-    //     }
-    // });
-// }
-
-
 const AWS = require('aws-sdk');
 const fs = require('fs');// Set the region
 
 AWS.config.update({
-    region: 'us-east-1',
-    accessKeyId: 'AKIAXGGJDX5IK3Z6DIF7',
-    secretAccessKey: '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'
+    region: '',
+    accessKeyId: '',
+    secretAccessKey: ''
 });// Create an S3 client
 
 const s3 = new AWS.S3();
